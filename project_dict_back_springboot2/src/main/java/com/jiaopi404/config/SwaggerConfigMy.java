f6551eaaa2bf0e5b14b2fc797eadb87664e78036
jiaopi404 <1240659804@qq.com>
2021-10-08 20:32:49 +0800

    update 部分整合 swagger 的代码

---
+++ project_dict_back_springboot2/src/main/java/com/jiaopi404/config/SwaggerConfigMy.java
@@ -0,0 +1,46 @@
+package com.jiaopi404.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfigMy {
+
+    //是否开启swagger，正式环境一般是需要关闭的，可根据springboot的多环境配置进行设置
+    @Value(value = "${swagger.enabled}")
+    Boolean swaggerEnabled;
+
+    public Docket createRestApi () {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(getApiInfo())
+                // 是否开启
+                .enable(swaggerEnabled)
+                .select()
+                // 扫描的路径包
+                .apis(RequestHandlerSelectors.basePackage("com.jiaopi404.controller"))
+                // 指定路径处理PathSelectors.any()代表所有的路径
+                .paths(PathSelectors.any())
+                .build()
+                .pathMapping("/");
+                // 扫描的路径
+    }
+
+    public ApiInfo getApiInfo () {
+        return new ApiInfoBuilder()
+                .title("jiaopi dict swagger2")
+                .description("jiaopi dict")
+                .contact(new Contact("jiaopi", "https://github.com/jiaopi404", "jiaopi404@163.com"))
+                .version("1.0.0")
+                .build();
+    }
+}
